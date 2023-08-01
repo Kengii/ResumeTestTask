8d199f427376a4bc01712f69b80550fdbbcbcedd
Kengii <kengii9431@gmail.com>
2023-08-01 18:00:03 +0300

    Project Created

---
+++ ResumeTestTask/ResumeTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResumeTestTask
+//
+//  Created by Владимир Данилович on 1.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
